bd28527390ee55357f133be780313cb594884d78
jono-openworld <53621272+jono-openworld@users.noreply.github.com>
2022-11-10 14:35:14 -0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-let show_hide_all_classes = true;
+
+ let showAllClassDescriptions = true;
 
 function toggleDisplay(divId) {
   var divElement;
@@ -7,10 +8,11 @@ function toggleDisplay(divId) {
   	divElement = document.getElementById(divId);
   	if (divElement.style.display == 'none')
   	{	
+		/* if hidden, then show */
 		divElement.style.display = 'block'; 
   	}
   	else 
-  	{	
+  	{	/* if shown, then hide */
   		divElement.style.display = 'none'; 
   	}
   } 
@@ -18,86 +20,24 @@ function toggleDisplay(divId) {
 
 function toggleShowHideAll(){
 
-	toggleShowHideDept("eng");
-	toggleShowHideDept("math");
-	
-	if(show_hide_all_classes){
-		show_hide_all_classes = false;
-	}
-	else{
-		show_hide_all_classes = true;
-	}
-}
-
-
-function toggleShowHideDept(dept) {
-
-	let courselists = document.getElementsByClassName(`${dept}-courselist`);
+	let courselists = document.getElementsByClassName("course-description");
 
 	for (i = 0; i < courselists.length; i++) {
-		if (show_hide_all_classes){
-			courselists[i].style.display="";
+		if (showAllClassDescriptions){
+			/* show */
+			courselists[i].style.display="block";
 		}
 		else {
+			/* hide */
 			courselists[i].style.display="none";
 		}
 	}
 
-
-	let coursenames = document.getElementsByClassName(`${dept}-coursename`);
-
-	for (i = 0; i < coursenames.length; i++) {
-		if (show_hide_all_classes){
-			coursenames[i].style.display="";
-		}
-		else {
-			coursenames[i].style.display="none";
-		}
+	/* toggle value */
+	if(showAllClassDescriptions) {
+		showAllClassDescriptions = false;
 	}
-
-	let courses = document.getElementsByClassName(`${dept}-course`);
-
-	for (i = 0; i < courses.length; i++) {
-		if (show_hide_all_classes){
-			courses[i].style.display="";
-		}
-		else {
-			courses[i].style.display="none";
-		}
-
-		courses[i].style.display="none";
+	else{
+		showAllClassDescriptions = true;
 	}
-}
-
-
-
-function search_coursenames() {
-    let input = document.getElementById('searchbar').value
-    input=input.toLowerCase();
-    let x = document.getElementsByClassName('coursename');
-      
-    for (i = 0; i < x.length; i++) { 
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display="none";
-        }
-        else {
-            x[i].style.display="";                 
-        }
-    }
-}
-
-
-function search_grades_ninth() {
-	var checkBox = document.getElementById("ninth-grade-checkbox");
-
-	let x = document.getElementsByClassName('ninth');
-      
-    for (i = 0; i < x.length; i++) { 
-		if (checkBox.checked == true){
-            x[i].style.display="";
-        }
-        else {
-            x[i].style.display="none";                 
-        }
-    }
 }
