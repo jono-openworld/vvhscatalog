3a893d206d33124419112915ebe9903821c85d73
jono-openworld <53621272+jono-openworld@users.noreply.github.com>
2023-01-20 07:24:07 -0700

    autoCommit 2023-01-20-@-07-24-06.966

--- javascript.js
+++ javascript.js
@@ -1,25 +1,345 @@
 
-let showAllClassDescriptions = true;
-let showNinthGrade = true;
-let showTenthGrade = true;
-let showEleventhGrade = true;
-let showTwelvethGrade = true;
+class Grades {
+    constructor(ninth, tenth, 
+                eleventh, twelveth) {
+      this.ninth = ninth;
+      this.tenth = tenth;
+      this.eleventh = eleventh;
+      this.twelveth = twelveth;
+    }
+}
 
-let showDeptCTE = true;
-let showDeptELA = true;
-let showDeptFA = true;
-let showDeptJROTC = true;
-let showDeptMATH = true;
-let showDeptMCNL = true;
-let showDeptPE = true;
-let showDeptSCI = true;
-let showDeptSPED = true;
-let showDeptSS = true;
-let showDeptSTU = true;
+class Departments {
+    constructor( cte, ela, fa, jrotc,
+                 math, mcnl, pe, sci,
+                 sped, ss, stu) {
+        this.cte = cte;
+        this.ela = ela;
+        this.fa = fa;
+        this.jrotc = jrotc;
+        this.math = math;
+        this.mcnl = mcnl;
+        this.pe = pe;
+        this.sci = sci;
+        this.sped = sped;
+        this.ss = ss;
+        this.stu = stu;
+    }
+}
+
+// initialize state with default checkbox values
+let state = {
+    departments: new Departments(true, true, true, true,
+                                 true, true, true, true, 
+                                 true, true, true),
+    grades: new Grades(true, true, true, true),
+    courseDetails: false,
+}
+
+const checkboxes = {
+    courseDetails: document.querySelector('#detailsCheck'),
+
+    deptCTE: document.querySelector('#Dept-CTE'),
+    deptELA: document.querySelector('#Dept-ELA'),
+    deptFA: document.querySelector('#Dept-FA'),
+    deptJROTC: document.querySelector('#Dept-JROTC'),
+    deptMATH: document.querySelector('#Dept-MATH'),
+    deptMCNL: document.querySelector('#Dept-MCNL'),
+    deptPE: document.querySelector('#Dept-PE'),
+    deptSCI: document.querySelector('#Dept-SCI'),
+    deptSPED: document.querySelector('#Dept-SPED'),
+    deptSS: document.querySelector('#Dept-SS'),
+    deptSTU: document.querySelector('#Dept-STU'),
+
+    gradeNinth: document.querySelector('#ninthCheck'),
+    gradeTenth: document.querySelector('#tenthCheck'),
+    gradeEleventh: document.querySelector('#eleventhCheck'),
+    gradeTwelveth: document.querySelector('#twelvethCheck'), 
+}
+
+init();
+
+function readState() {
+    var obj;
+
+    try {
+        obj = JSON.parse(sessionStorage.state);
+    } catch (e) {
+        console.error(e instanceof SyntaxError);
+        console.error(e.message);
+        console.error(e.name);
+        console.error(e.fileName);
+        console.error(e.lineNumber);
+        console.error(e.columnNumber);
+        console.error(e.stack);
+    }
+
+    return obj;
+}
+
+function writeState(obj) {
+    sessionStorage.setItem('state', JSON.stringify(obj));
+}
+
+function init() {
+    console.log('init()');
+
+    var newState = readState();
+    if (newState != null){
+        state = newState;
+    }
+    else {
+        writeState(state);
+    }
+    updatePage();  
+}
+
+function updatePage() {
+
+    if (checkboxes != null){
+
+        // Departments checkboxes
+        if (checkboxes.deptCTE != null) {
+            checkboxes.deptCTE.checked = state.departments.cte;
+        }
+        if (checkboxes.deptELA != null) {
+            checkboxes.deptELA.checked = state.departments.ela;
+        }
+        if (checkboxes.deptFA != null) {
+            checkboxes.deptFA.checked = state.departments.fa;
+        }
+        if (checkboxes.deptJROTC != null) {
+            checkboxes.deptJROTC.checked = state.departments.jrotc;
+        }
+        if (checkboxes.deptMATH != null) {
+            checkboxes.deptMATH.checked = state.departments.math;
+        }
+        if (checkboxes.deptMCNL != null) {
+            checkboxes.deptMCNL.checked = state.departments.mcnl;
+        }
+        if (checkboxes.deptPE != null) {
+            checkboxes.deptPE.checked = state.departments.pe;
+        }
+        if (checkboxes.deptSCI != null){
+            checkboxes.deptSCI.checked = state.departments.sci;
+        }
+        if (checkboxes.deptSPED != null) {
+            checkboxes.deptSPED.checked = state.departments.sped;
+        }
+        if (checkboxes.deptSS != null){
+            checkboxes.deptSS.checked = state.departments.ss;
+        }
+        if (checkboxes.deptSTU != null) {
+            checkboxes.deptSTU.checked = state.departments.stu;
+        }  
+        
+        // Grades checkboxes
+        if (checkboxes.gradeNinth != null){
+            checkboxes.gradeNinth.checked = state.grades.ninth;
+        }
+        if (checkboxes.gradeTenth != null){
+            checkboxes.gradeTenth.checked = state.grades.tenth;
+        }
+        if (checkboxes.gradeEleventh != null){
+            checkboxes.gradeEleventh.checked = state.grades.eleventh;
+        }
+        if (checkboxes.gradeTwelveth != null){
+            checkboxes.gradeTwelveth.checked = state.grades.twelveth;
+        }
+
+        // Course Details checkbox
+        if (checkboxes.courseDetails != null){
+            checkboxes.courseDetails.checked = state.courseDetails;
+        }
+        
+    }
+
+    // Show or hide departments
+    showHideDepartments();   
+
+    // Show or hide grade levels
+    showHideGrades();
+
+    // Show or hide course details
+    showHideCourseDetails();
+
+}
+
+//////////////////////////////////////
+// Department event listeners
+//
+//
+checkboxes.deptCTE.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.departments.cte = !state.departments.cte;
+        writeState(state);
+        updatePage();
+    }  
+});
+
+checkboxes.deptELA.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.departments.ela = !state.departments.ela;
+        writeState(state);
+        updatePage();
+    }  
+});
+
+checkboxes.deptFA.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.departments.fa = !state.departments.fa;
+        writeState(state);
+        updatePage();
+    }  
+});
+
+checkboxes.deptJROTC.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.departments.jrotc = !state.departments.jrotc;
+        writeState(state);
+        updatePage();
+    }  
+});
+
+checkboxes.deptMATH.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.departments.math = !state.departments.math;
+        writeState(state);
+        updatePage();
+    }  
+});
+
+checkboxes.deptMCNL.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.departments.mcnl = !state.departments.mcnl;
+        writeState(state);
+        updatePage();
+    }  
+});
+
+checkboxes.deptPE.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.departments.pe = !state.departments.pe;
+        writeState(state);
+        updatePage();
+    }  
+});
+
+checkboxes.deptSCI.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.departments.sci = !state.departments.sci;
+        writeState(state);
+        updatePage();
+    }  
+});
+
+checkboxes.deptSS.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.departments.ss = !state.departments.ss;
+        writeState(state);
+        updatePage();
+    }  
+});
+
+checkboxes.deptSPED.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.departments.sped = !state.departments.sped;
+        writeState(state);
+        updatePage();
+    }  
+});
+
+checkboxes.deptSTU.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.departments.stu = !state.departments.stu;
+        writeState(state);
+        updatePage();
+    }  
+});
+
+/////////////////////////////////
+// Grades event listener
+//
+checkboxes.gradeNinth.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.grades.ninth = !state.grades.ninth;
+        writeState(state);
+        updatePage();
+    }  
+});
+
+checkboxes.gradeTenth.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.grades.tenth = !state.grades.tenth;
+        writeState(state);
+        updatePage();
+    }  
+});
+
+checkboxes.gradeEleventh.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.grades.eleventh = !state.grades.eleventh;
+        writeState(state);
+        updatePage();
+    }  
+});
+
+checkboxes.gradeTwelveth.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.grades.twelveth = !state.grades.twelveth;
+        writeState(state);
+        updatePage();
+    }  
+});
 
 
+/////////////////////////////////
+// Course details event listener
+//
+checkboxes.courseDetails.addEventListener('change', function () {
+    state = readState();
+
+    if (state != null){
+        state.courseDetails = !state.courseDetails;
+        writeState(state);
+        updatePage();
+    }  
+});
 
 
+////////////////////////////////////
+// Show / Hide elements functions
+//
 function toggleDisplay(divId) {
   var divElement;
   if (document.getElementById(divId)) 
@@ -37,12 +357,12 @@ function toggleDisplay(divId) {
   } 
 }
 
-function toggleShowHideAll(){
 
+function showHideCourseDetails(){
 	let coursesList = document.getElementsByClassName("course-details");
 
 	for (i = 0; i < coursesList.length; i++) {
-		if (showAllClassDescriptions){
+		if (state.courseDetails){
 			/* show */
 			coursesList[i].style.display="block";
 		}
@@ -51,228 +371,103 @@ function toggleShowHideAll(){
 			coursesList[i].style.display="none";
 		}
 	}
-
-	/* toggle value */
-	if(showAllClassDescriptions) {
-		showAllClassDescriptions = false;
-	}
-	else{
-		showAllClassDescriptions = true;
-	}
 }
 
-function toggleShowHideGrade(gradeName){
-    switch(gradeName) {
-        case "ninth-grade":
-            showNinthGrade = !showNinthGrade;
-            break;
-        
-        case "tenth-grade":
-            showTenthGrade = !showTenthGrade;
-            break;
-           
-        case "eleventh-grade":
-            showEleventhGrade = !showEleventhGrade;
-                break;
-        
-        case "twelveth-grade":
-            showTwelvethGrade = !showTwelvethGrade;
-            break;
-        
-        default:
-            break;
-    }
-    
+function getGradesSelector(grades){
     let showSelector = '';
-    let hideSelector = '.grade-level';
     
-    if (showNinthGrade == true) {
+    if (grades.ninth == true) {
         showSelector += '.ninth-grade,';
     }
-    if (showTenthGrade == true) {
+    if (grades.tenth == true) {
         showSelector += '.tenth-grade,';
     }
-    if (showEleventhGrade == true) {
+    if (grades.eleventh == true) {
         showSelector += '.eleventh-grade,';
     }
-    if (showTwelvethGrade == true) {
+    if (grades.twelveth == true) {
         showSelector += '.twelveth-grade,';
     }
     // Remove the final character from selectors
     showSelector = showSelector.slice(0, -1);
-    
-    
-    /* Hide all courses with class ".grade-level" first */
-    try {
-        console.log(hideSelector);
-        var coursesList = document.querySelectorAll(hideSelector);
-        var count = 0;
-        
-        if (coursesList != null) {
-            /* Loop over courses in list, each course.style.diplay to block */
-            coursesList.forEach((course) => {
-              course.style.display="none";
-              count++;
-            });
-        }
-        
-        console.log(count);
 
-    }
-    catch (e) {
-        console.error(e instanceof SyntaxError);
-        console.error(e.message);
-        console.error(e.name);
-        console.error(e.fileName);
-        console.error(e.lineNumber);
-        console.error(e.columnNumber);
-        console.error(e.stack);
-    }    
-    
-    /* Then select courses to show
-       For example, the selector statement . . .
-       ".ninth-grade, .tenth-grade, .eleventh-grade, .twelveth-grade"
-       ==
-       .ninth-grade || .tenth-grade || .eleventh-grade || .twelveth-grade
-       
-       selects any element with one or more of these classes.
-    */
-    try {
-        console.log(showSelector);
-        if (showSelector !== ''){           
-            var coursesList = document.querySelectorAll(showSelector);
-            var count = 0;
-            
-            if (coursesList != null) {
-                /* Loop over courses in list, each course.style.diplay to block */
-                coursesList.forEach((course) => {
-                  course.style.display="block";
-                  count++;
-                });
-                console.log(count);
-            }
-        }
-    }
-    catch (e) {
-        console.error(e instanceof SyntaxError);
-        console.error(e.message);
-        console.error(e.name);
-        console.error(e.fileName);
-        console.error(e.lineNumber);
-        console.error(e.columnNumber);
-        console.error(e.stack);
-    }
-  
+    return showSelector;
 }
 
+function showHideGrades(){
+    let showSelector = getGradesSelector(state.grades);
+    let hideSelector = '.grade-level';
 
-function toggleShowHideDept(deptName){
-    switch(deptName) {
-        case "Dept-CTE":
-            showDeptCTE = !showDeptCTE;
-            break;
+    showHide(showSelector, hideSelector);  
+}
 
-        case "Dept-ELA":
-            showDeptELA = !showDeptELA;
-            break;
-
-        case "Dept-FA":
-            showDeptFA = !showDeptFA;
-            break;
-
-        case "Dept-JROTC":
-            showDeptJROTC = !showDeptJROTC;
-            break;
-
-        case "Dept-MATH":
-            showDeptMATH = !showDeptMATH;
-            break;
-
-        case "Dept-MCNL":
-            showDeptMCNL = !showDeptMCNL;
-            break;
-    
-        case "Dept-PE":
-            showDeptPE = !showDeptPE;
-            break;
-
-        case "Dept-SCI":
-            showDeptSCI = !showDeptSCI;
-            break;
-        
-        case "Dept-SPED":
-            showDeptSPED = !showDeptSPED;
-            break;
-        
-        case "Dept-SS":
-            showDeptSS = !showDeptSS;
-            break;
-
-        case "Dept-STU":
-            showDeptSTU = !showDeptSTU;
-            break;
-
-        default:
-            break;
-    }
-    
+function getDepartmentsSelector(departments){
     let showSelector = '';
-    let hideSelector = '.department-div';
 
-    if (showDeptCTE == true) {
+    if (departments.cte == true) {
         showSelector += '.Dept-CTE,';
     }
 
-    if (showDeptELA == true) {
+    if (departments.ela == true) {
         showSelector += '.Dept-ELA,';
     }
 
-    if (showDeptFA == true) {
+    if (departments.fa == true) {
         showSelector += '.Dept-FA,';
     }
 
-    if (showDeptJROTC == true) {
+    if (departments.jrotc == true) {
         showSelector += '.Dept-JROTC,';
     }
 
-    if (showDeptMATH == true) {
+    if (departments.math == true) {
         showSelector += '.Dept-MATH,';
     }
 
-    if (showDeptMCNL == true) {
+    if (departments.mcnl == true) {
         showSelector += '.Dept-MCNL,';
     }
 
-    if (showDeptPE == true) {
+    if (departments.pe == true) {
         showSelector += '.Dept-PE,';
     }
 
-    if (showDeptSCI == true) {
+    if (departments.sci == true) {
         showSelector += '.Dept-SCI,';
     }
 
-    if (showDeptSS == true) {
+    if (departments.ss == true) {
         showSelector += '.Dept-SS,';
     }
 
-    if (showDeptSTU == true) {
+    if (departments.stu == true) {
         showSelector += '.Dept-STU,';
     }
 
     // Remove the final character from selectors
     showSelector = showSelector.slice(0, -1);
     
-    
-    /* Hide all departments with ".department-div" first */
+    return showSelector;
+}
+
+
+function showHideDepartments(){
+    let showSelector = getDepartmentsSelector(state.departments);
+    let hideSelector = '.department-div'; 
+
+    showHide(showSelector, hideSelector);
+}
+
+
+function showHide(showSelector, hideSelector) {
     try {
         console.log(hideSelector);
-        var departmentList = document.querySelectorAll(hideSelector);
+        var list = document.querySelectorAll(hideSelector);
         var count = 0;
         
-        if (departmentList != null) {
-            /* Loop over departments in list, each department.style.diplay to block */
-            departmentList.forEach((department) => {
-              department.style.display="none";
+        if (list != null) {
+            list.forEach((element) => {
+              element.style.display="none";
               count++;
             });
         }
@@ -290,22 +485,15 @@ function toggleShowHideDept(deptName){
         console.error(e.stack);
     }    
     
-    /* Then select departments to show
-       For example, the selector statement . . .
-       ".Dept-CTE, .Dept-MATH, .Dept-SCI"
-       ==
-       .Dept-CTE || .Dept-MATH || .Dept-SCI
-       selects any element with one or more of these classes.
-    */
+
     try {
         console.log(showSelector);
         if (showSelector !== ''){           
-            var departmentList = document.querySelectorAll(showSelector);
+            var list = document.querySelectorAll(showSelector);
             var count = 0;
             
-            if (departmentList != null) {
-                /* Loop over departments in list, each department.style.diplay to block */
-                departmentList.forEach((department) => {
+            if (list != null) {
+                list.forEach((department) => {
                   department.style.display="block";
                   count++;
                 });
@@ -321,7 +509,7 @@ function toggleShowHideDept(deptName){
         console.error(e.lineNumber);
         console.error(e.columnNumber);
         console.error(e.stack);
-    }
-  
+    } 
 }
+
 
